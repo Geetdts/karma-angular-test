f9d531d9bdd1934db208530850dc86bbdc8ab69c
Gayan Geethanjana <102275293+Geetdts@users.noreply.github.com>
2024-08-06 15:44:19 +0400

    login form without validation
    complete login form without validations

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, HttpClientModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+
+export class LoginComponent {
+
+  loginObj: Login;
+  constructor(private http: HttpClient,private router:Router){
+    this.loginObj = new Login();
+  }
+    //code for login with backend API 
+  // onLogin() {
+
+  //   this.http.post('https:/localhost/api/v1/login', this.loginObj).subscribe(
+  //     (res:any)=>{
+  //     if(res.result) {
+  //       alert("Login Success");
+  //       localStorage.setItem('karmaTestToken', res.data.token)
+  //       this.router.navigateByUrl('/dashboard')
+  //     } else {
+  //       alert(res.message)
+  //     }
+  //   })
+  // }
+  onLogin() {
+    let email: string = "geeth@karma.com";
+    let password: string = "geeth@karma";
+      
+        if(this.loginObj.EmailId== email && this.loginObj.Password == password ) {
+          alert("Login Success");
+          this.router.navigateByUrl('/dashboard')
+        } else {
+          alert("Login faild")
+        }
+     
+    }
+}
+
+export class Login  { 
+  EmailId: string;
+    Password: string;
+    constructor() {
+      this.EmailId = '';
+      this.Password = '';
+    } 
+}
